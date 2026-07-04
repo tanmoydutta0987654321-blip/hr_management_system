0228b641d8a444f1d0ecfa245bfc60be2f73f7e2
Abhishek Malakar <abhishekmalakar631@gmail.com>
2026-07-04 12:46:23 +0530

    connected database and maintained system setups

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask ,render_template,request,redirect,url_for
-
+from db import get_db_connection
 app = Flask(__name__)
 
 @app.route("/")
---
+++ config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME")
